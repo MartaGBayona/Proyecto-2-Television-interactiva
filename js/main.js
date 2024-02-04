086c2219ce629dfb201e8ba07b0a0a8f19f87d3d
MartaGBayona <marta.g.bayona@outlook.es>
2024-02-04 10:58:26 +0100

    data interactive ok

--- js/main.js
+++ js/main.js
@@ -9,11 +9,63 @@ let arrayButtons = Array.from(buttons);
 
 let screen = document.getElementById("screen");
 
+//TV On/Off
+
+let isTvOn = false;
+
+remoteControl.addEventListener('click', (e) => {
+    isTvOn = !isTvOn;
+    if (isTvOn) {
+        screen.style.backgroundColor = '#222';
+        enableRemote();
+    } else {
+        screen.style.backgroundColor = 'black';
+        disableRemote();
+    }
+});
+
+function enableRemote() {
+    remoteButtons.forEach(button => {
+        button.disabled = false;
+    });
+}
+
+function disableRemote() {
+    remoteButtons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
+//Interactive buttons
+
 arrayButtons.map(
     item => {
-        item.addEventListener("click", (evento) =>{
+        item.addEventListener("click", (e) =>{
             screen.classList.remove(screen.classList[screen.classList.length - 1])
-            screen.classList.add("canal" + evento.target.id.slice(-1))
+            screen.classList.add("canal" + e.target.id.slice(-1))
         })
     }
 )
+
+//Interactive date
+
+function updateTime() {
+    const data = document.getElementById("data");
+    const now = new Date();
+
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true
+    };
+    const dateTimeString = now.toLocaleString('en-En', options);
+    data.textContent = dateTimeString;
+}
+
+// Actualizar la hora cada segundo
+setInterval(updateTime, 1000);
